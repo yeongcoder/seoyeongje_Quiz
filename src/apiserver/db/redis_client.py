db7e8704612071afb46f91ae911dc00550764683
yeongcoder <syj10433@gmail.com>
2025-05-18 19:50:22 +0900

    redis접속 포트 기본포트로 변경

--- src/apiserver/db/redis_client.py
+++ src/apiserver/db/redis_client.py
@@ -1,4 +1,4 @@
 import redis.asyncio as redis
 
 # Redis 클라이언트 설정
-redis_client = redis.Redis(host="localhost", port=32770, db=0, decode_responses=True)
+redis_client = redis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
